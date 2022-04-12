c74385fde6be9655908c66bd405e9d3d14af64bc
pitzzahh <araopj2191@gmail.com>
2022-04-12 23:11:21 +0800

    renamed class from NumberAnswerException to NumberResponseException

---
+++ src/main/java/exception/NumberResponseException.java
@@ -0,0 +1,11 @@
+package exception;
+
+/**
+ * class used for exception handling on number answers.
+ * This class extends the {@code RuntimeException}
+ */
+public class NumberResponseException extends RuntimeException {
+    public NumberResponseException() {
+        super("Number Response Not Allowed");
+    }
+}
