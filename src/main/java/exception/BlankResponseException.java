6a5c40ebcf6326e00d7a0189a3ced3a5e3c79251
pitzzahh <araopj2191@gmail.com>
2022-04-12 23:10:39 +0800

    renamed class from BlankAnswerException to BlankResponseException

---
+++ src/main/java/exception/BlankResponseException.java
@@ -0,0 +1,11 @@
+package exception;
+
+/**
+ * class used for exception handling on blank answers.
+ * This class extends the {@code RuntimeException}
+ */
+public class BlankResponseException extends RuntimeException {
+    public BlankResponseException() {
+        super("Blank Response Not Allowed");
+    }
+}
